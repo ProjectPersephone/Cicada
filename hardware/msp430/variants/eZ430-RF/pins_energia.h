6e34e57a386d3d8ab4bda3d61267d9919c7ee3b8
Stefan Schauer <s-schauer1@ti.com>
2012-06-13 15:50:02 +0200

    Several updates to pinning and add of FR5739

--- hardware/msp430/variants/eZ430-RF/pins_energia.h
+++ hardware/msp430/variants/eZ430-RF/pins_energia.h
@@ -2,10 +2,10 @@
   ************************************************************************
   *	pins_energia.h
   *
-  *	Energia core files for MSP430
+  *	Energia core files for MSP430F2274
   *		Copyright (c) 2012 Robert Wessels. All right reserved.
   *
-  *     Contribution: Rei VILO
+  *     Contribution: StefanSch
   *
   ***********************************************************************
   Derived from:
@@ -37,17 +37,12 @@
 #endif
 
 #if defined(__MSP430_HAS_USCI__)
-static const uint8_t SS   = 8;  /* P3.0 */
-static const uint8_t SCK  = 7;  /* P3.3 */
-static const uint8_t MOSI = 15; /* P3.1 */
-static const uint8_t MISO = 14; /* P3.2 */
-#endif
-
-#if defined(__MSP430_HAS_USI__)
-static const uint8_t SS   = 8;  /* P2.0 */
-static const uint8_t SCK  = 7;  /* P1.5 */
-static const uint8_t MOSI = 14; /* P1.6 */
-static const uint8_t MISO = 15; /* P1.7 */
+static const uint8_t SS     = 9;  /* P3.0 */
+static const uint8_t SCK    = 12; /* P3.3 */
+static const uint8_t MOSI   = 10; /* P3.1 */
+static const uint8_t MISO   = 11; /* P3.2 */
+static const uint8_t TWISDA = 10; /* P3.1 */
+static const uint8_t TWISCL = 11; /* P3.2 */
 #endif
 
 static const uint8_t A0 = 0;
@@ -59,19 +54,34 @@ static const uint8_t A5 = 5;
 static const uint8_t A6 = 6;
 static const uint8_t A7 = 7;
 static const uint8_t A10 = 10; // special. This is the internal temp sensor
+static const uint8_t A11 = 11; // 
+static const uint8_t A12 = 12; // 
+static const uint8_t A13 = 13; // 
+static const uint8_t A14 = 14; // 
+static const uint8_t A15 = 15; // 
 
-//                      +-\/-+
-//               VCC   1|    |20  GND
-//         (A0)  P1.0  2|    |19  XIN
-//         (A1)  P1.1  3|    |18  XOUT
-//         (A2)  P1.2  4|    |17  TEST
-//         (A3)  P1.3  5|    |16  RST#
-//         (A4)  P1.4  6|    |15  P1.7  (A7) (SCL) (MISO) depends on chip
-//         (A5)  P1.5  7|    |14  P1.6  (A6) (SDA) (MOSI)
-//               P2.0  8|    |13  P2.5
-//               P2.1  9|    |12  P2.4
-//               P2.2 10|    |11  P2.3
-//                      +----+
+//                      +---\/---+
+//               GND   1|        |40  P2.5
+//           XOUT/P2.7 2|        |39  DVCC
+//            XIN/P2.6 3|        |38  DVCC
+//               DVSS  4|        |37  TEST
+//               RST#  5|        |36  P1.7
+//       (A0)    P2.0  6|        |35  P1.6
+//       (A1)    P2.1  7|        |34  P1.5
+//       (A2)    P2.2  8|        |33  P1.4
+//       (A5)    P3.0  9|        |32  P1.3
+//               P3.1 10|        |31  P1.2
+//               P3.2 11|        |30  P1.1
+//               P3.3 12|        |29  P1.0
+//               AVSS 13|        |28  P2.4    (A4)
+//               AVCC 14|        |27  P2.3    (A3)
+//               P4.0 15|        |26  P3.7
+//               P4.1 16|        |25  P3.6
+//               P4.2 17|        |24  P3.5
+//       (A12)   P4.3 18|        |23  P3.4
+//       (A13)   P4.4 19|        |22  P4.7
+//       (A14)   P4.5 20|        |21  P4.6    (A15)
+//                      +--------+
 //
 
 // Pin names based on the silkscreen
@@ -79,14 +89,13 @@ static const uint8_t A10 = 10; // special. This is the internal temp sensor
 
 static const uint8_t P2_7 = 2;
 static const uint8_t P2_6 = 3;
-
 static const uint8_t P2_0 = 6;
 static const uint8_t P2_1 = 7;
 static const uint8_t P2_2 = 8;
 static const uint8_t P3_0 = 9;
 static const uint8_t P3_1 = 10;
 static const uint8_t P3_2 = 11;
-
+static const uint8_t P3_3 = 12;
 static const uint8_t P4_0 = 15;
 static const uint8_t P4_1 = 16;
 static const uint8_t P4_2 = 17;
@@ -110,7 +119,6 @@ static const uint8_t P1_4 = 33;
 static const uint8_t P1_5 = 34;
 static const uint8_t P1_6 = 35;
 static const uint8_t P1_7 = 36;
-
 static const uint8_t P2_5 = 40;
 
 static const uint8_t RED_LED = 2;
@@ -210,103 +218,134 @@ const uint16_t port_to_output[] = {
  */
 const uint8_t digital_pin_to_timer[] = {
 	NOT_ON_TIMER, /*  dummy */
-	NOT_ON_TIMER, /*  1 - VCC */
-	NOT_ON_TIMER, /*  2 - P1.0 */
-	T0A0,         /*  3 - P1.1, note: A0 output cannot be used with analogWrite */
-	T0A1,         /*  4 - P1.2 */
-	NOT_ON_TIMER, /*  5 - P1.3 */
-	NOT_ON_TIMER, /*  6 - P1.4 note: special case. Leaving as no timer due to difficulty determining if available */
-	T0A0,         /*  7 - P1.5 note: A0 output cannot be used with analogWrite  */
-#if defined(__MSP430_HAS_T1A3__) 
-	T1A0,         /*  8 - P2.0 note: A0 output cannot be used with analogWrite */
-	T1A1,         /*  9 - P2.1 */
-	T1A1,         /* 10 - P2.3 */
-	T1A0,         /* 11 - P2.4 note: A0 output cannot be used with analogWrite  */
-	T1A2,         /* 12 - P2.5 */
-	T1A2,         /* 13 - P2.6 */
-#else
-	NOT_ON_TIMER, /*  8 - P2.0 */
-	NOT_ON_TIMER, /*  9 - P2.1 */
-	NOT_ON_TIMER, /* 10 - P2.3 */
-	NOT_ON_TIMER, /* 11 - P2.4 */
-	NOT_ON_TIMER, /* 12 - P2.5 */
-	NOT_ON_TIMER, /* 13 - P2.6 */
-#endif
-	T0A1,         /* 14 - P1.6 */
-	NOT_ON_TIMER, /* 15 - P1.7 */
-	NOT_ON_TIMER, /* 16 - /RESET */  
-	NOT_ON_TIMER, /* 17 - TEST */  
-	NOT_ON_TIMER, /* 18 - XOUT - P2.7 */
-	T0A1,         /* 18 - XIN - P2.6: */
-	NOT_ON_TIMER, /* 20 - GND */
+	NOT_ON_TIMER, /*  1 - GND */
+	NOT_ON_TIMER, /*  2 - XOUT - P2.7 */
+	NOT_ON_TIMER, /*  3 - XIN - P2.6: */
+	NOT_ON_TIMER, /*  4 - GND */
+	NOT_ON_TIMER, /*  5 - /RESET */  
+	NOT_ON_TIMER, /*  6 - P2.0 */
+	NOT_ON_TIMER, /*  7 - P2.1 */
+	T0A0,         /*  8 - P2.2 */
+	NOT_ON_TIMER, /*  9 - P3.0 */
+	NOT_ON_TIMER, /* 10 - P3.1 */
+	NOT_ON_TIMER, /* 11 - P3.2 */
+	NOT_ON_TIMER, /* 12 - P3.3 */
+	NOT_ON_TIMER, /* 13 - AVSS */
+	NOT_ON_TIMER, /* 14 - AVCC */
+	T0B0,         /* 15 - P4.0 */
+	T0B1,         /* 16 - P4.1 */
+	T0B2,         /* 17 - P4.2 */
+	T0B0,         /* 18 - P4.3 */
+	T0B1,         /* 19 - P4.4 */
+	T0B2,         /* 20 - P4.5 */
+	NOT_ON_TIMER, /* 21 - P4.6 */
+	NOT_ON_TIMER, /* 22 - P4.7 */
+	NOT_ON_TIMER, /* 23 - P3.4 */
+	NOT_ON_TIMER, /* 24 - P3.5 */
+	NOT_ON_TIMER, /* 25 - P3.6 */
+	NOT_ON_TIMER, /* 26 - P3.7 */
+	NOT_ON_TIMER, /* 27 - P2.3 */
+	NOT_ON_TIMER, /* 28 - P2.4 */
+	NOT_ON_TIMER, /* 29 - P1.0 */
+	T0A0,         /* 30 - P1.1 */
+	T0A1,         /* 31 - P1.2 */
+	T0A2,         /* 32 - P1.3 */
+	NOT_ON_TIMER, /* 33 - P1.4 */
+	T0A0,         /* 34 - P1.5 */
+	T0A1,         /* 35 - P1.6 */
+	T0A2,         /* 36 - P1.7 */
+	NOT_ON_TIMER, /* 37 - TEST */
+	NOT_ON_TIMER, /* 38 - DVCC */
+	NOT_ON_TIMER, /* 39 - DVSS */
+	NOT_ON_TIMER, /* 40 - P2.5 */
 };
 
 const uint8_t digital_pin_to_port[] = {
 	NOT_A_PIN, /* dummy */
-	NOT_A_PIN, /* 1 */
-	P2, /* 2 */
-	P2, /* 3 */
-	NOT_A_PIN, /* 4 */
-	NOT_A_PIN, /* 5 */
-	P2, /* 6 */
-	P2, /* 7 */
-	P2, /* 8 */
-	P3, /* 9 */
-	P3, /* 10 */
-	P3, /* 11 */
-	P3, /* 12 */
-	NOT_A_PIN, /* 13 */
-	NOT_A_PIN, /* 14 */
-	P4, /* 15 */
-	P4, /* 16 */
-	P4, /* 17 */
-	P4, /* 18 */
-	P4, /* 19 */
-	P4, /* 20 */
-	P4, /* 21 */
-	P4, /* 22 */
-	P3, /* 23 */
-	P3, /* 24 */
-	P3, /* 25 */
-	P3, /* 26 */
-	P2, /* 27 */
-	P2, /* 28 */
-	P1, /* 29 */
-	P1, /* 30 */
-	P1, /* 31 */
-	P1, /* 32 */
-	P1, /* 33 */
-	P1, /* 34 */
-	P1, /* 35 */
-	P1, /* 36 */
-	NOT_A_PIN, /* 37 */
-	NOT_A_PIN, /* 38 */
-	NOT_A_PIN, /* 39 */
-	P2, /* 40 */
+	NOT_A_PIN, /*  1 - GND */
+	NOT_A_PIN, /*  2 - XOUT - P2.7 */
+	NOT_A_PIN, /*  3 - XIN - P2.6: */
+	NOT_A_PIN, /*  4 - GND */
+	NOT_A_PIN, /*  5 - /RESET */  
+	P2,        /*  6 - P2.0 */
+	P2,        /*  7 - P2.1 */
+	P2,        /*  8 - P2.2 */
+	P3,        /*  9 - P3.0 */
+	P3,        /* 10 - P3.1 */
+	P3,        /* 11 - P3.2 */
+	P3,        /* 12 - P3.3 */
+	NOT_A_PIN, /* 13 - AVSS */
+	NOT_A_PIN, /* 14 - AVCC */
+	P4,        /* 15 - P4.0 */
+	P4,        /* 16 - P4.1 */
+	P4,        /* 17 - P4.2 */
+	P4,        /* 18 - P4.3 */
+	P4,        /* 19 - P4.4 */
+	P4,        /* 20 - P4.5 */
+	P4,        /* 21 - P4.6 */
+	P4,        /* 22 - P4.7 */
+	P3,        /* 23 - P3.4 */
+	P3,        /* 24 - P3.5 */
+	P3,        /* 25 - P3.6 */
+	P3,        /* 26 - P3.7 */
+	P2,        /* 27 - P2.3 */
+	P2,        /* 28 - P2.4 */
+	P1,        /* 29 - P1.0 */
+	P1,        /* 30 - P1.1 */
+	P1,        /* 31 - P1.2 */
+	P1,        /* 32 - P1.3 */
+	P1,        /* 33 - P1.4 */
+	P1,        /* 34 - P1.5 */
+	P1,        /* 35 - P1.6 */
+	P1,        /* 36 - P1.7 */
+	NOT_A_PIN, /* 37 - TEST */
+	NOT_A_PIN, /* 38 - DVCC */
+	NOT_A_PIN, /* 39 - DVSS */
+	P2,        /* 40 - P2.5 */
 };
 
 const uint8_t digital_pin_to_bit_mask[] = {
 	NOT_A_PIN, /* 0,  pin count starts at 1 */
-	NOT_A_PIN, /* 1,  VCC */
-	BV(0),     /* 2,  port P1.0 */
-	BV(1),     /* 3,  port P1.1 */
-	BV(2),     /* 4,  port P1.2 */
-	BV(3),     /* 5,  port P1.3*/
-	BV(4),     /* 6,  port P1.4 */
-	BV(5),     /* 7,  port P1.5 */
-	BV(0),     /* 8,  port P2.0 */
-	BV(1),     /* 9,  port P2.1 */
-	BV(2),     /* 10, port P2.2 */
-	BV(3),     /* 11, port P2.3 */
-	BV(4),     /* 12, port P2.4 */
-	BV(5),     /* 13, port P2.5 */
-	BV(6),     /* 14, port P1.6 */
-	BV(7),     /* 15, port P1.7 */
-	NOT_A_PIN, /* 16, RST */
-	NOT_A_PIN, /* 17, TEST */
-	NOT_A_PIN, /* 18, XOUT */
-	NOT_A_PIN, /* 19, XIN */
-	NOT_A_PIN, /* 20, GND */
+	NOT_A_PIN, /*  1 - GND */
+	NOT_A_PIN, /*  2 - XOUT - P2.7 */
+	NOT_A_PIN, /*  3 - XIN - P2.6: */
+	NOT_A_PIN, /*  4 - GND */
+	NOT_A_PIN, /*  5 - /RESET */  
+	BV(0),     /*  6 - P2.0 */
+	BV(1),     /*  7 - P2.1 */
+	BV(2),     /*  8 - P2.2 */
+	BV(0),     /*  9 - P3.0 */
+	BV(1),     /* 10 - P3.1 */
+	BV(2),     /* 11 - P3.2 */
+	BV(3),     /* 12 - P3.3 */
+	NOT_A_PIN, /* 13 - AVSS */
+	NOT_A_PIN, /* 14 - AVCC */
+	BV(0),     /* 15 - P4.0 */
+	BV(1),     /* 16 - P4.1 */
+	BV(2),     /* 17 - P4.2 */
+	BV(3),     /* 18 - P4.3 */
+	BV(4),     /* 19 - P4.4 */
+	BV(5),     /* 20 - P4.5 */
+	BV(6),     /* 21 - P4.6 */
+	BV(7),     /* 22 - P4.7 */
+	BV(4),     /* 23 - P3.4 */
+	BV(5),     /* 24 - P3.5 */
+	BV(6),     /* 25 - P3.6 */
+	BV(7),     /* 26 - P3.7 */
+	BV(3),     /* 27 - P2.3 */
+	BV(4),     /* 28 - P2.4 */
+	BV(0),     /* 29 - P1.0 */
+	BV(1),     /* 30 - P1.1 */
+	BV(2),     /* 31 - P1.2 */
+	BV(3),     /* 32 - P1.3 */
+	BV(4),     /* 33 - P1.4 */
+	BV(5),     /* 34 - P1.5 */
+	BV(6),     /* 35 - P1.6 */
+	BV(7),     /* 36 - P1.7 */
+	NOT_A_PIN, /* 37 - TEST */
+	NOT_A_PIN, /* 38 - DVCC */
+	NOT_A_PIN, /* 39 - DVSS */
+	BV(5),     /* 40 - P2.5 */
 };
 #endif
 #endif
