eea1b37b851163927940e29e696da97990bc4650
Stefan Schauer <s-schauer1@ti.com>
2012-07-10 15:49:33 +0200

    TWI Basic Implementation

--- hardware/msp430/cores/msp430/wiring_digital.c
+++ hardware/msp430/cores/msp430/wiring_digital.c
@@ -115,7 +115,6 @@ void digitalWrite(uint8_t pin, uint8_t val)
 	uint8_t bit = digitalPinToBitMask(pin);
 	uint8_t port = digitalPinToPort(pin);
 	volatile uint8_t *out;
-	volatile uint8_t *sel;
 
 	if (port == NOT_A_PORT) return;
 
